1aefd6a2a338d8457cc486ff01862f0272b3ead6
Israel Pablo Coelho <israel.coelho-ext@viavarejo.com.br>
2024-10-01 11:16:07 -0300

    feat: Generated script to setup git alias for the  most used commands

---
+++ git-aliases.sh
@@ -0,0 +1,6 @@
+git config --global alias.co checkout &&
+git config --global alias.br branch &&
+git config --global alias.st status &&
+git config --global alias.sw switch &&
+git config --global alias.loga 'log --oneline' &&
+git config --global alias.cm 'commit -m'
